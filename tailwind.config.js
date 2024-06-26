8b50702225bd8e830456345fcc46f81beca5bb75
Geiner <geiner.martinez39145@ucaldas.edu.co>
2024-06-25 20:44:08 -0500

    3 actualizacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
+// tailwind.config.js
+
 module.exports = {
   content: [
     './src/**/*.html',
-    './src/**/*.ejs',
-    './src/views/**/*.ejs',
-    './src/**/*.js',  
+    './src/**/*.ejs', // Aquí se incluyen todos los archivos .ejs dentro de /src
+    './src/**/*.js',
     './src/**/*.jsx',
     './src/**/*.ts',
     './src/**/*.tsx',
     './public/**/*.html',
   ],
+  darkMode: false, // O 'media' o 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
